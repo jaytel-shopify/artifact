04c1feefd80bedab9cd214642a97a8652ae856e5
Jesper Vos <jesper.vos@shopify.com>
2025-11-26 16:05:23 +0100

    toggle easing

--- components/layout/header/ViewToggle.tsx
+++ components/layout/header/ViewToggle.tsx
@@ -11,20 +11,20 @@ export default function ViewToggle() {
   return (
     <div className="grid grid-cols-2 grid-rows-[100%] bg-primary rounded-button p-0.5 h-10 border-[0.5px] border-border">
       <div
-        className={`button-primary rounded-button-inner col-start-1 col-span-1 row-start-1 row-span-1 transition-transform duration-200 ${isExplore ? "translate-x-0" : "translate-x-full"}`}
+        className={`button-primary rounded-button-inner col-start-1 col-span-1 row-start-1 row-span-1 transition-transform duration-500 ease-spring ${isExplore ? "translate-x-0 [&:has(~_.projects:hover)]:scale-x-106 [&:has(~_.projects:hover)]:scale-y-96 [&:has(~_.projects:hover)]:translate-x-[4%]" : "translate-x-full [&:has(~_.explore:hover)]:scale-x-106 [&:has(~_.explore:hover)]:scale-y-96 [&:has(~_.explore:hover)]:translate-x-[94%]"}`}
       ></div>
       <Link
         href="/"
-        className={`relative flex items-center justify-center px-3 rounded-button-inner text-small transition-colors duration-200 h-full row-start-1 row-span-1 col-start-1 ${
-          isExplore ? "text-white" : "text-foreground hover:text-foreground"
+        className={`explore relative flex items-center justify-center px-3 rounded-button-inner text-small transition-colors duration-200 h-full row-start-1 row-span-1 col-start-1 ${
+          isExplore ? "text-white" : "text-foreground"
         }`}
       >
         Explore
       </Link>
       <Link
         href="/projects"
-        className={`relative flex items-center justify-center px-3 rounded-button-inner text-small transition-colors duration-200 h-full row-start-1 row-span-1 col-start-2 ${
-          isProjects ? "text-white" : "text-foreground hover:text-foreground"
+        className={`projects relative flex items-center justify-center px-3 rounded-button-inner text-small transition-colors duration-200 h-full row-start-1 row-span-1 col-start-2 ${
+          isProjects ? "text-white" : "text-foreground"
         }`}
       >
         Projects
